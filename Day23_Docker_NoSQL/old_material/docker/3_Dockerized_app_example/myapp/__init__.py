b43a9d962af58595da9e81b33b6c7e91f0154bfb
Sasha Kasper <acasper@student.42berlin.de>
2025-10-27 10:40:20 +0100

    added Day23_Docker_NoSQL to DSR44

---
+++ Day23_Docker_NoSQL/old_material/docker/3_Dockerized_app_example/myapp/__init__.py
@@ -0,0 +1,13 @@
+import numpy as np
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def random_median():
+    a = np.random.random(12)
+    return str(np.median(a))
+
+@app.route('/hello')
+def hello():
+    return "this is another route"
